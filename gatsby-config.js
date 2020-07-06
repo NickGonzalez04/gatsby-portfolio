91849bf18183d942643173ed141293b58cde35ca
Nicholas Gonzalez <gonzaleznick5@gmail.com>
2020-07-06 15:32:52 -0400

    changed favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `NICHOLAS GONZALEZ | GONZALEZ`,
+    title: `NICHOLAS GONZALEZ | SOFTWARE ENGINEER`,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    author: `Nicholas Gonzalez`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
